2e1083eebd8d4246796596af41445833e51654bc
Robert Moore <robert.ben.moore@gmail.com>
2018-09-28 15:27:47 -0700

    ASP Week 1 Complete

---
+++ Week 1/Part 2/community/community/Models/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace community.Models
+{
+    public class ContactModel
+    {
+        string name;
+        string emailAddress;
+        string message;
+    }
+}
